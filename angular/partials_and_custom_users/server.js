1d2808ee8ac32793af589c84d9685ef6a2ecf235
Jerrod Quintana <jerrodq2@yahoo.com>
2017-09-26 14:21:49 -0500

    Added comments to the projects "partials_and_custom_users" and "mongoose_dashboard_madularized" to further my understanding of what the line "app.use(express.static(path.join(__dirname + '/client')))" means. Also made slight changes to express_commands word doc, just backing up all my projects

--- angular/partials_and_custom_users/server.js
+++ angular/partials_and_custom_users/server.js
@@ -5,3 +5,4 @@ var express = require('express'),
 
 app.use(express.static(path.join(__dirname + '/client')));
 app.use(express.static(path.join(__dirname + '/bower_components')));
+//the two above lines are important and used in full mean. The "path.join()" part basically includes them in the directory we're currently in, the root directory. So, in index.html, you'll notice I include two angular files, both are within the bower_components folder, normally I'd have to specify the file like so: "bower_components/angular/angular.js", but instead, I do this: "angular/angular.js", this is because of the above lines. I include the files/folders in the bower_components folder and in the client folder with the root directy, so as far as node is concerned,index.html within the client folder is in the root directory, not the client folder. 
