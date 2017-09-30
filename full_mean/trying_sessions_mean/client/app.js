7442d99f633984024caafbc23debdd66f0da3053
Jerrod Quintana <jerrodq2@yahoo.com>
2017-09-29 23:15:53 -0500

    altered the project trying_sessions_mean, altered and commented the backend to improve and demonstrate session use, and added the functionality of determining how long a session lasts

--- full_mean/trying_sessions_mean/client/app.js
+++ full_mean/trying_sessions_mean/client/app.js
@@ -1,5 +1,10 @@
 var app = angular.module('myApp', ['ngRoute', 'ngCookies'])
 
+app.config(['$locationProvider', function($locationProvider) {
+  $locationProvider.hashPrefix('');
+}]);
+// I add the above code block because, as of angular 1.6.0, the route default hash-prefix changes from an emptry string ('') to the bang ('!'), which messes up my routes and causes angular-route to not work properly. The code block above restores the previous behavior so I don't have figure out how to change the rest of my routes accordingly. This line should be put after you declare your angularjs module, and before your angular routes below.
+
 app.config(function ($routeProvider) {
 
 $routeProvider
